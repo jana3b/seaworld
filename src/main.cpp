d56641cdbe429be266fa6378e7852f82cf444196
Jana <jana@janin.laptop>
2024-03-28 00:57:17 +0100

    added plane

--- src/main.cpp
+++ src/main.cpp
@@ -188,8 +188,78 @@ int main() {
     pointLight.linear = 0.35f;
     pointLight.quadratic = 0.032f;
 
+    //********************************************************************************************************
+    // STONE PLANE
 
-    //Skybox implementation
+
+    Shader planeShader("resources/shaders/plane.vs", "resources/shaders/plane.fs");
+
+    // set up vertex data (and buffer(s)) and configure vertex attributes
+    // ------------------------------------------------------------------
+    float vertices[] = {
+            // positions                    // texture coords
+            0.5f,  0.5f, 0.0f, 10.0f, 10.0f, // top right         1  1
+            0.5f, -0.5f, 0.0f, 10.0f, 0.0f, // bottom right      1  0
+            -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, // bottom left   0  0
+            -0.5f,  0.5f, 0.0f, 0.0f, 10.0f  // top left      0  1
+    };
+    unsigned int indices[] = {
+            0, 1, 3, // first triangle
+            1, 2, 3  // second triangle
+    };
+
+    unsigned int VBO, VAO, EBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    // position attribute
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    // texture coord attribute
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+
+    // load and create a texture
+    // -------------------------
+    unsigned int texture1;
+    glGenTextures(1, &texture1);
+    glBindTexture(GL_TEXTURE_2D, texture1); // all upcoming GL_TEXTURE_2D operations now have effect on this texture object
+    // set the texture wrapping parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	// set texture wrapping to GL_REPEAT (default wrapping method)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    // set texture filtering parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    // load image, create texture and generate mipmaps
+    int width, height, nrChannels;
+    // The FileSystem::getPath(...) is part of the GitHub repository so we can find files on any IDE/platform; replace it with your own image path.
+    unsigned char *data = stbi_load(FileSystem::getPath("resources/textures/flagstone.png").c_str(), &width, &height, &nrChannels, 0);
+    if (data)
+    {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else
+    {
+        std::cout << "Failed to load texture" << std::endl;
+    }
+    stbi_image_free(data);
+
+
+
+    //********************************************************************************************************
+    // SKYBOX
+
 
     Shader skyboxShader("resources/shaders/skybox.vs", "resources/shaders/skybox.fs");
 
@@ -267,8 +337,10 @@ int main() {
     // draw in wireframe
     //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
-    // render loop
-    // -----------
+
+    //********************************************************************************************************
+    // RENDER LOOP
+
     while (!glfwWindowShouldClose(window)) {
         // per-frame time logic
         // --------------------
@@ -332,14 +404,30 @@ int main() {
             DrawImGui(programState);
 
 
-        //glm::mat4 skyboxView = programState->camera.GetViewMatrix();
-        glm::mat4 skyboxProjection = glm::perspective(glm::radians(programState->camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+        //draw stone tiles
+
+        glBindTexture(GL_TEXTURE_2D, texture1);
+        planeShader.use();
+
+        glm::mat4 planeModel = glm::mat4(1.0f);
+        planeModel = glm::rotate(planeModel, glm::radians(90.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+        planeModel = glm::scale(planeModel, glm::vec3(100.0f, 100.0f, 1.0f));
+
+        planeShader.setMat4("model", planeModel);
+        planeShader.setMat4("view", view);
+        planeShader.setMat4("projection", projection);
+
+        glBindVertexArray(VAO);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
 
 
         // draw skybox as last
+
         glDepthFunc(GL_LEQUAL);  // change depth function so depth test passes when values are equal to depth buffer's content
         skyboxShader.use();
         glm::mat4 skyboxView = glm::mat4(glm::mat3(programState->camera.GetViewMatrix())); // remove translation from the view matrix
+        glm::mat4 skyboxProjection = glm::perspective(glm::radians(programState->camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
         skyboxShader.setMat4("view", skyboxView);
         skyboxShader.setMat4("projection", skyboxProjection);
         // skybox cube
